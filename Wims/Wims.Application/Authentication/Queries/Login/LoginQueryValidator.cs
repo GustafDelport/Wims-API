9fd772febea36cf0bb66a661a2837bb6580d3509
Gustaf Delport <gustafdelport@gmail.com>
2022-10-01 13:35:25 +0200

    Changed validators

--- Wims/Wims.Application/Authentication/Queries/Login/LoginQueryValidator.cs
+++ Wims/Wims.Application/Authentication/Queries/Login/LoginQueryValidator.cs
@@ -7,13 +7,7 @@ namespace Wims.Application.Authentication.Queries.Login
         public LoginQueryValidator()
         {
             RuleFor(x => x.Email).EmailAddress();
-            RuleFor(x => x.Password).NotEmpty().WithMessage("Your password cannot be empty.")
-                .MinimumLength(8).WithMessage("Your password length must be at least 8.")
-                .MaximumLength(16).WithMessage("Your password length must not exceed 16.")
-                .Matches(@"[A-Z]+").WithMessage("Your password must contain at least one uppercase letter.")
-                .Matches(@"[a-z]+").WithMessage("Your password must contain at least one lowercase letter.")
-                .Matches(@"[0-9]+").WithMessage("Your password must contain at least one number.")
-                .Matches(@"[\!\?\*\.\@]+").WithMessage("Your password must contain at least one (! ? * . @).");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Your password cannot be empty.");
         }
     }
 }
